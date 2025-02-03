aef6ea5cb55a700fa181d0cb8c4208a782d69af4
ndrsonya <sofia.altusar-anderson@suomenterveysravinto.fi>
2025-02-03 15:42:54 +0200

    Remove unused imports from test file

--- src/test/java/com/example/recipybook/JPAtest.java
+++ src/test/java/com/example/recipybook/JPAtest.java
@@ -1,33 +1,20 @@
 package com.example.recipybook;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 
 import com.example.recipybook.domain.*;
 
-import static org.assertj.core.api.Assertions.*;
+
 
 /**
  * Testing the web layer
- * 
  * Spring application context is started, but without the server
  */
 @RunWith(SpringRunner.class)
